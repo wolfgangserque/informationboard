12ac6dc441d455a09b495afb20d97d98c4679456
wolfgangserque <wolfgangserque@gmail.com>
2024-06-07 18:16:36 +0700

    ganti sistem pake PHP, halaman2 baru, button2 dilinkkan

---
+++ components/logo.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" style="position: absolute;
+        left: 50%; transform: translateX(-50%);top:150px;" href="index.php"><img src="img/logo.png"></a>
+      </div>
+    </nav>
+  </div>
---
+++ library/buttonback.js
@@ -0,0 +1,5 @@
+           // Tambahkan event listener ke tombol
+            document.getElementById('backButton').addEventListener('click', function() {
+                // Fungsi untuk kembali ke halaman sebelumnya
+                window.history.back();
+            });
